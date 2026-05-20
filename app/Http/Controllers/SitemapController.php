87529a6a1838edfa98b1f3bc1202d79f75262501
najaandika <naja.andika3000@gmail.com>
2026-05-21 00:33:51 +0700

    fix: sitemap controller and view

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use Illuminate\Http\Response;
 
 class SitemapController extends Controller
 {
     public function index()
     {
-        $products = Product::where('status', 'available')
-            ->orderBy('updated_at', 'desc')
-            ->get();
+        $content = view('sitemap')->render();
 
-        $content = view('sitemap', compact('products'))->render();
+        // Remove any leading whitespace/BOM before XML declaration
+        $content = ltrim($content);
 
         return response($content, 200)
-            ->header('Content-Type', 'application/xml');
+            ->header('Content-Type', 'application/xml; charset=UTF-8')
+            ->header('X-Robots-Tag', 'noindex');
     }
 }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ url('/') }}</loc>
@@ -15,19 +15,11 @@
     <url>
         <loc>{{ url('/login') }}</loc>
         <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
+        <priority>0.3</priority>
     </url>
     <url>
         <loc>{{ url('/register') }}</loc>
         <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
+        <priority>0.3</priority>
     </url>
-    @foreach($products as $product)
-    <url>
-        <loc>{{ url('/landing/products/' . $product->id . '/checkout') }}</loc>
-        <lastmod>{{ $product->updated_at->toW3cString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    @endforeach
 </urlset>
